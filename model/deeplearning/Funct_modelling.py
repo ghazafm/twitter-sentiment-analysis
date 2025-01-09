d123ad52da93a932ec0521e5b05f4878622de4b0
ghazafm <fauzan.ghaza@gmail.com>
2025-01-10 01:43:47 +0700

    update deep learning

---
+++ model/deeplearning/Funct_modelling.py
@@ -0,0 +1,111 @@
+import pandas as pd
+
+def undersampling(x):
+    x_p = x[x['label']=='Positive']
+    x_n = x[x['label']=='Negative']
+
+    x_temp = x_p.sample(x_n.label.count(),random_state=42)
+    x_under = pd.concat([x_temp,x_n],axis=0,ignore_index=True)
+    return x_under
+
+def tfidf_vec(data):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+
+    vectorizer = TfidfVectorizer()
+    data = vectorizer.fit_transform(data)
+    data = data.toarray()
+    data = pd.DataFrame(data, columns=vectorizer.get_feature_names_out())
+    return data
+
+# train test split
+def bagi_data(x, y):
+    from sklearn.model_selection import train_test_split
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=2
+    )
+    return x_train, x_test, y_train, y_test
+
+def preprocessing(data,tag=False):
+    data = undersampling(data)
+    tag = []
+    if tag:
+        tag = data[data.columns[-20:]]
+
+    X = data['no_stopwords']
+    y = data['label']
+
+    X = tfidf_vec(X)
+    if tag:
+        X = pd.concat([X,tag],axis=1)
+
+    x_train, x_test, y_train, y_test = bagi_data(X,y)
+
+    # if tag:
+    #     tag_train = x_train[x_train.columns[-20:]]
+    #     tag_test = x_test[x_test.columns[-20:]]
+    
+    #     x_train = x_train.drop(x_train[x_train.columns[-20:]],axis=1)
+    #     x_test = x_test.drop(x_test[x_test.columns[-20:]],axis=1)
+
+    #     x_train = pd.concat([x_train,tag_train],axis=1)
+    #     x_test = pd.concat([x_test,tag_test],axis=1)
+
+    return x_train, x_test, y_train, y_test
+
+
+# Metric Scorer
+def accuracy_score(y_test, y_pred):
+    from sklearn.metrics import accuracy_score
+
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+
+def f1_score_pos(x, y):
+    from sklearn.metrics import f1_score
+
+    f1 = f1_score(x, y, pos_label="Positive")
+    return f1
+
+
+def f1_score_neg(x, y):
+    from sklearn.metrics import f1_score
+
+    f1 = f1_score(x, y, pos_label="Negative")
+    return f1
+
+
+def precission_pos(x, y):
+    from sklearn.metrics import precision_score
+
+    precision = precision_score(x, y, pos_label="Positive")
+    return precision
+
+
+def precission_neg(x, y):
+    from sklearn.metrics import precision_score
+
+    precision = precision_score(x, y, pos_label="Negative")
+    return precision
+
+
+def recall_pos(x, y):
+    from sklearn.metrics import recall_score
+
+    recall = recall_score(x, y, "Positive")
+    return recall
+
+
+def recall_neg(x, y):
+    from sklearn.metrics import recall_score
+
+    recall = recall_score(x, y, "Negative")
+    return recall
+
+
+def all_report(x, y):
+    from sklearn.metrics import classification_report
+
+    report = classification_report(x, y)
+    return report
